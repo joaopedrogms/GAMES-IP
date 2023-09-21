454b0b31e220049d4a29d3f1e78ec0f733c4c415
zrThawan <136650797+zrThawan@users.noreply.github.com>
2023-09-21 15:31:36 -0300

    HUD
    hud concluido!

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import os
 import pygame as pg
+from pygame.locals import *
 
 if not pg.font:
     print("Warning, fonts disabled")
@@ -8,7 +9,6 @@ if not pg.mixer:
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 data_dir = os.path.join(main_dir, "media")
-
 
 def load_image(name, colorkey=None, scale=0.3):
     fullpath = os.path.join(data_dir, 'img')
@@ -27,6 +27,43 @@ def load_image(name, colorkey=None, scale=0.3):
 
     return image
 
+class HUD(pg.sprite.Sprite):
+    def __init__(self, character):
+        pg.sprite.Sprite.__init__(self)
+        self.character = character
+        self.heart_image = load_image('coracao.png', scale=0.06)
+        self.chave_dourada_image = load_image('chave_dourada.png', scale=0.065)
+        self.chave_azul_image = load_image('chave_azul.png', scale=0.065)
+        self.rect = self.heart_image.get_rect()
+        self.rect.topleft = (10, 10)
+        self.key_font = pg.font.Font(None, 30)
+
+    def update(self):
+        self.image = pg.Surface((1900, 1900), pg.SRCALPHA)
+        self.image.fill((0, 0, 0, 0))
+
+        #corações
+        heart_x = 10
+        for _ in range(self.character.hp):
+            self.image.blit(self.heart_image, (heart_x, 10))
+            heart_x += 40
+
+        #chaves amarelas
+        chave_dourada_rect = self.chave_dourada_image.get_rect()
+        chave_dourada_rect.topleft = (self.image.get_width() - 1000, 10)
+        self.image.blit(self.chave_dourada_image, chave_dourada_rect)
+        chave_dourada_count = self.key_font.render(str(self.character.chaves_coletadas), True, (255, 255, 50))
+        self.image.blit(chave_dourada_count, (
+        chave_dourada_rect.right + 10, chave_dourada_rect.centery - chave_dourada_count.get_height() // 2))
+
+        #chaves azuis
+        chave_azul_rect = self.chave_azul_image.get_rect()
+        chave_azul_rect.topleft = (self.image.get_width() - 1000, 50)
+        self.image.blit(self.chave_azul_image, chave_azul_rect)
+        chave_azul_count = self.key_font.render(str(self.character.chaves_azuis_coletadas), True, (150, 150, 150))
+        self.image.blit(chave_azul_count, (chave_azul_rect.right + 10, chave_azul_rect.centery - chave_azul_count.get_height() // 2))
+
+
 class Character(pg.sprite.Sprite):
 
     def __init__(self):
@@ -39,6 +76,9 @@ class Character(pg.sprite.Sprite):
         self.hp = 3
         self.attack = 1
         self.speed = 3
+        self.chaves_coletadas = 100
+        self.chaves_azuis_coletadas = 0
+        self.jaula_coletada = 0
 
     def update(self, width, heigth):
         top_value = self.rect[1]
@@ -48,9 +88,9 @@ class Character(pg.sprite.Sprite):
             self.image_upper = self.rect.copy()
 
         self._walk(width)
-        self._jump()
+        self._jump(heigth)
 
-    def _jump(self):
+    def _jump(self, height):
         up_pressed = pg.key.get_pressed()[pg.K_UP]
         w_pressed = pg.key.get_pressed()[pg.K_w]
         jump_height = 10
@@ -61,8 +101,8 @@ class Character(pg.sprite.Sprite):
             self.jump_height = -jump_height
 
         if self.jump:
-            if self.rect.bottom >= 960:
-                self.rect.y = 959 - self.rect.height
+            if self.rect.bottom >= height:
+                self.rect.y = (height - 1) - self.rect.height
                 self.jump = False
             else:
                 self.rect.y += self.jump_height
@@ -94,15 +134,69 @@ class Character(pg.sprite.Sprite):
             else:
                 self.image = load_image('sprite_llama.png')
 
-def main():
+class Key(pg.sprite.Sprite):
+    def __init__(self, x, y, imagem, scale):
+        pg.sprite.Sprite.__init__(self)
+        self.figura = imagem
+        self.scale = scale
+        self.original_image = load_image(self.figura)
+        self.image = pg.transform.scale(self.original_image, (int(self.original_image.get_width() * scale), int(self.original_image.get_height() * scale)))  # Aplica a escala à imagem
+        self.rect = self.image.get_rect()
+        
+        self.rect.center = (x, y)
+        self.collected = False
 
+    def update(self, character):
+        if not self.collected and self.figura == "chave_dourada.png":
+            if self.rect.colliderect(character.rect):
+                self.collected = True
+                character.chaves_coletadas += 1
+    
+        if not self.collected and self.figura == "chave_azul.png":
+            if self.rect.colliderect(character.rect):
+                self.collected = True
+                character.chaves_azuis_coletadas += 1
+
+        if not self.collected:
+            if character.chaves_coletadas == 3 and character.chaves_azuis_coletadas == 3 and self.rect.colliderect(character.rect):
+                self.collected = True
+                character.jaula_coletada += 1
+
+    def draw(self, screen):
+        if not self.collected:
+            screen.blit(self.image, self.rect)
+
+def main():
     pg.init()
-    screen = pg.display.set_mode((1280, 960), pg.SCALED)
+    screen = pg.display.set_mode((1080, 760), pg.SCALED)
     pg.display.set_caption("Llama simulator")
 
+    width = screen.get_size()[0]
+    height = screen.get_size()[1]
+
     background = pg.Surface(screen.get_size())
-    background = background.convert()
-    background.fill((128, 128, 128))
+    background = load_image('background.jpeg')
+    background = pg.transform.scale(background,(width,height))
+
+    # chave
+    keys_group = pg.sprite.Group()
+    key_gold1 = Key(600, 620, "chave_dourada.png", 0.25)
+    key_gold2 = Key(400, 620, "chave_dourada.png", 0.25)
+    key_gold3 = Key(500, 620, "chave_dourada.png", 0.25)
+    imagem_chave = Key(930, 30, "chave_dourada.png", 0.25)
+
+    Key_blue1 = Key(800, 620, "chave_azul.png", 0.25)
+    Key_blue2 = Key(700, 620, "chave_azul.png", 0.25)
+    Key_blue3 = Key(900, 620, "chave_azul.png", 0.25)
+    imagem_chave_azul = Key(930, 70, "chave_azul.png", 0.25)
+
+    jaula = Key(300, 600, "image-remove.png", 0.5)
+    imagem_jaula = Key(920, 130, "image-remove.png", 0.5)
+    
+    keys_group.add(
+    key_gold1, key_gold2, key_gold3,
+    Key_blue1, Key_blue2, Key_blue3,
+    jaula)
 
     screen.blit(background, (0, 0))
     print(pg.display.get_surface().get_size())
@@ -111,6 +205,9 @@ def main():
     pocoyo = Character()
     allsprites = pg.sprite.RenderPlain((pocoyo))
     clock = pg.time.Clock()
+
+    hud = HUD(pocoyo)
+    hud_group = pg.sprite.Group(hud)
 
     going = True
     while going:
@@ -124,15 +221,20 @@ def main():
         elif going:
             pocoyo.update(screen.get_size()[0], screen.get_size()[1])
 
-        allsprites.update(screen.get_size()[0], screen.get_size()[1])
+        keys_group.update(pocoyo)
 
         screen.blit(background, (0, 0))
+        for key in keys_group:
+            key.draw(screen)
+        allsprites.update(screen.get_size()[0], screen.get_size()[1])
         allsprites.draw(screen)
+
+        hud_group.update()
+        hud_group.draw(screen)
 
         pg.display.flip()
 
     pg.quit()
-
 
 if __name__ == "__main__":
     main()
