ea10d5b5bf41273889c7893a2acd1e5bcbe4e0a6
zrThawan <136650797+zrThawan@users.noreply.github.com>
2023-09-21 16:24:24 -0300

    alterações de sprites

--- game.py
+++ game.py
@@ -34,6 +34,7 @@ class HUD(pg.sprite.Sprite):
         self.heart_image = load_image('coracao.png', scale=0.06)
         self.chave_dourada_image = load_image('chave_dourada.png', scale=0.065)
         self.chave_azul_image = load_image('chave_azul.png', scale=0.065)
+        self.morango_image = load_image('morango.png', scale=0.16)
         self.rect = self.heart_image.get_rect()
         self.rect.topleft = (10, 10)
         self.key_font = pg.font.Font(None, 30)
@@ -48,19 +49,25 @@ class HUD(pg.sprite.Sprite):
             self.image.blit(self.heart_image, (heart_x, 10))
             heart_x += 40
 
+        #morangos
+        morango_rect = self.morango_image.get_rect()
+        morango_rect.topleft = (self.image.get_width() - 990, 10)
+        self.image.blit(self.morango_image, morango_rect)
+        morango_count = self.key_font.render(str(self.character.morangos_coletados), True, (255, 0, 0))
+        self.image.blit(morango_count, (morango_rect.right + 10, morango_rect.centery))
+
         #chaves amarelas
         chave_dourada_rect = self.chave_dourada_image.get_rect()
-        chave_dourada_rect.topleft = (self.image.get_width() - 1000, 10)
+        chave_dourada_rect.topleft = (self.image.get_width() - 1000, 50)
         self.image.blit(self.chave_dourada_image, chave_dourada_rect)
-        chave_dourada_count = self.key_font.render(str(self.character.chaves_coletadas), True, (255, 255, 50))
-        self.image.blit(chave_dourada_count, (
-        chave_dourada_rect.right + 10, chave_dourada_rect.centery - chave_dourada_count.get_height() // 2))
+        chave_dourada_count = self.key_font.render(str(self.character.chaves_coletadas) + ' / 1', True, (255, 255, 50))
+        self.image.blit(chave_dourada_count, (chave_dourada_rect.right + 10, chave_dourada_rect.centery - chave_dourada_count.get_height() // 2))
 
         #chaves azuis
         chave_azul_rect = self.chave_azul_image.get_rect()
-        chave_azul_rect.topleft = (self.image.get_width() - 1000, 50)
+        chave_azul_rect.topleft = (self.image.get_width() - 1000, 90)
         self.image.blit(self.chave_azul_image, chave_azul_rect)
-        chave_azul_count = self.key_font.render(str(self.character.chaves_azuis_coletadas), True, (150, 150, 150))
+        chave_azul_count = self.key_font.render(str(self.character.chaves_azuis_coletadas) + ' / 1', True, (60, 135, 210))
         self.image.blit(chave_azul_count, (chave_azul_rect.right + 10, chave_azul_rect.centery - chave_azul_count.get_height() // 2))
 
 
@@ -73,12 +80,13 @@ class Character(pg.sprite.Sprite):
         self.image_upper = self.rect.copy()
         self.looking = True
         self.jump = False
-        self.hp = 3
+        self.hp = 5
         self.attack = 1
         self.speed = 3
-        self.chaves_coletadas = 100
+        self.chaves_coletadas = 0
         self.chaves_azuis_coletadas = 0
         self.jaula_coletada = 0
+        self.morangos_coletados = 0
 
     def update(self, width, heigth):
         top_value = self.rect[1]
@@ -175,23 +183,19 @@ def main():
     height = screen.get_size()[1]
 
     background = pg.Surface(screen.get_size())
-    background = load_image('background.jpeg')
-    background = pg.transform.scale(background,(width,height))
+    background = load_image('background.png')
+    background = pg.transform.scale(background, (width, height))
 
-    # chave
     keys_group = pg.sprite.Group()
     key_gold1 = Key(600, 620, "chave_dourada.png", 0.25)
     key_gold2 = Key(400, 620, "chave_dourada.png", 0.25)
     key_gold3 = Key(500, 620, "chave_dourada.png", 0.25)
-    imagem_chave = Key(930, 30, "chave_dourada.png", 0.25)
 
     Key_blue1 = Key(800, 620, "chave_azul.png", 0.25)
     Key_blue2 = Key(700, 620, "chave_azul.png", 0.25)
     Key_blue3 = Key(900, 620, "chave_azul.png", 0.25)
-    imagem_chave_azul = Key(930, 70, "chave_azul.png", 0.25)
 
-    jaula = Key(300, 600, "image-remove.png", 0.5)
-    imagem_jaula = Key(920, 130, "image-remove.png", 0.5)
+    jaula = Key(300, 678, "image-remove.png", 1)
     
     keys_group.add(
     key_gold1, key_gold2, key_gold3,
