8c68392ac2b288a53a5f8e0f991edf4bb6d3b944
zrThawan <136650797+zrThawan@users.noreply.github.com>
2023-09-22 08:05:50 -0300

    main

--- game.py
+++ game.py
@@ -2,14 +2,8 @@ import os
 import pygame as pg
 from pygame.locals import *
 
-if not pg.font:
-    print("Warning, fonts disabled")
-if not pg.mixer:
-    print("Warning, sound disabled")
-
 main_dir = os.path.split(os.path.abspath(__file__))[0]
-data_dir = os.path.join(main_dir, "media")
-
+data_dir = os.path.join(main_dir, 'media')
 
 def load_image(name, colorkey=None, scale=0.3):
     fullpath = os.path.join(data_dir, 'img')
@@ -28,6 +22,51 @@ def load_image(name, colorkey=None, scale=0.3):
 
     return image
 
+class HUD(pg.sprite.Sprite):
+    def __init__(self, character):
+        pg.sprite.Sprite.__init__(self)
+        self.character = character
+        self.heart_image = load_image('coracao.png', scale=0.06)
+        self.chave_dourada_image = load_image('chave_dourada.png', scale=0.065)
+        self.chave_azul_image = load_image('chave_azul.png', scale=0.065)
+        self.morango_image = load_image('morango.png', scale=0.16)
+        self.rect = self.heart_image.get_rect()
+        self.rect.topleft = (10, 10)
+        self.key_font = pg.font.Font('daydream.ttf', 15)
+
+    def update(self):
+        self.image = pg.Surface((1900, 1900), pg.SRCALPHA)
+        self.image.fill((0, 0, 0, 0))
+
+        # corações
+        heart_x = 10
+        for _ in range(self.character.hp):
+            self.image.blit(self.heart_image, (heart_x, 10))
+            heart_x += 40
+
+        # morangos
+        morango_rect = self.morango_image.get_rect()
+        morango_rect.topleft = (self.image.get_width() - 920, 10)
+        self.image.blit(self.morango_image, morango_rect)
+        morango_count = self.key_font.render(str(self.character.morangos_coletados + (self.character.vidas_adicionadas * 10)), True, (255, 0, 0))
+        self.image.blit(morango_count, (morango_rect.right + 10, morango_rect.centery))
+
+        # chaves amarelas
+        chave_dourada_rect = self.chave_dourada_image.get_rect()
+        chave_dourada_rect.topleft = (self.image.get_width() - 930, 50)
+        self.image.blit(self.chave_dourada_image, chave_dourada_rect)
+        chave_dourada_count = self.key_font.render(str(self.character.chaves_coletadas), True, (255, 255, 50))
+        self.image.blit(chave_dourada_count, (
+        chave_dourada_rect.right + 10, chave_dourada_rect.centery - chave_dourada_count.get_height() // 2))
+
+        # chaves azuis
+        chave_azul_rect = self.chave_azul_image.get_rect()
+        chave_azul_rect.topleft = (self.image.get_width() - 930, 90)
+        self.image.blit(self.chave_azul_image, chave_azul_rect)
+        chave_azul_count = self.key_font.render(str(self.character.chaves_azuis_coletadas), True, (60, 135, 210))
+        self.image.blit(chave_azul_count,
+                        (chave_azul_rect.right + 10, chave_azul_rect.centery - chave_azul_count.get_height() // 2))
+
 class Character(pg.sprite.Sprite):
 
     def __init__(self):
@@ -38,39 +77,51 @@ class Character(pg.sprite.Sprite):
         self.looking = True
         self.jump = False
         self.hp = 3
+        self.vidas_adicionadas = 0
         self.attack = 1
         self.speed = 3
         self.chaves_coletadas = 0
         self.chaves_azuis_coletadas = 0
-        self.jaula_coletada = 0
+        self.jaula_coletada = False
+        self.morangos_coletados = 0
 
-    def update(self, width, heigth):
+    def update(self, width, height, grounds):
         top_value = self.rect[1]
         if top_value == 0:
             self.rect = self.rect.move(
                 0, pg.display.get_surface().get_size()[1] - self.rect[3])
             self.image_upper = self.rect.copy()
 
-        self._walk(width)
-        self._jump(heigth)
+        if self.morangos_coletados >= 10:
+            self.hp += 1
+            self.vidas_adicionadas += 1
+            self.morangos_coletados -= 10
 
-    def _jump(self, height):
+        self._walk(width)
+        colisao_chao = pg.sprite.spritecollide(self, grounds, False)
+        self._jump(height, colisao_chao)
+
+    def _jump(self, height, colisao):
         up_pressed = pg.key.get_pressed()[pg.K_UP]
         w_pressed = pg.key.get_pressed()[pg.K_w]
         jump_height = 10
         gravity = 0.3
 
-        if (w_pressed or up_pressed) and not self.jump:
-            self.jump = True
-            self.jump_height = -jump_height
+        if colisao:
+            self.rect.y = colisao[0].rect.top - self.rect.height
+            self.jump = False
+        else:
+            if (w_pressed or up_pressed) and not self.jump:
+                self.jump = True
+                self.jump_height = -jump_height
 
-        if self.jump:
-            if self.rect.bottom >= height:
-                self.rect.y = (height - 1) - self.rect.height
-                self.jump = False
-            else:
-                self.rect.y += self.jump_height
-                self.jump_height += gravity
+            if self.jump:
+                if self.rect.bottom >= height:
+                    self.rect.y = height - self.rect.height
+                    self.jump = False
+                else:
+                    self.rect.y += self.jump_height
+                    self.jump_height += gravity
 
     def _walk(self, width):
         d_pressed = pg.key.get_pressed()[pg.K_d]
@@ -98,80 +149,149 @@ class Character(pg.sprite.Sprite):
             else:
                 self.image = load_image('sprite_llama.png')
 
+class FemLlama(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        pg.sprite.Sprite.__init__(self)
+        self.image = load_image('fem_llama.png', scale=0.3)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.jump = False
+
+    def _jump(self, height, grounds):
+        colisao = pg.sprite.spritecollide(self, grounds, False)
+        jump_height = 19
+        gravity = 1.2
+
+        if colisao:
+            self.image = load_image('fem_llama.png', scale=0.3)
+            self.rect.y = colisao[0].rect.top - self.rect.height
+            self.jump = False
+        else:
+            self.image = load_image('fem_llama_pulo.png', scale=0.3)
+            if not self.jump:
+                self.jump = True
+                self.jump_height = -jump_height
+
+            if self.jump:
+                if self.rect.bottom >= height:
+                    self.rect.y = height - self.rect.height
+                    self.jump = False
+                else:
+                    self.rect.y += self.jump_height
+                    self.jump_height += gravity
+
+class Morango(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        pg.sprite.Sprite.__init__(self)
+        self.image = load_image('morango.png', scale=0.16)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
 class Key(pg.sprite.Sprite):
     def __init__(self, x, y, imagem, scale):
         pg.sprite.Sprite.__init__(self)
         self.figura = imagem
         self.scale = scale
         self.original_image = load_image(self.figura)
-        self.image = pg.transform.scale(self.original_image, (int(self.original_image.get_width() * scale), int(self.original_image.get_height() * scale)))  # Aplica a escala à imagem
+        self.image = pg.transform.scale(self.original_image, (int(self.original_image.get_width() * scale), int(self.original_image.get_height() * scale)))
         self.rect = self.image.get_rect()
-        
+
         self.rect.center = (x, y)
-        self.collected = False  # Inicialmente, o objeto não foi coletada
+        self.collected = False
 
     def update(self, character):
-        if not self.collected and self.figura == "chave_dourada.png":
+        if not self.collected and self.figura == 'chave_dourada.png':
             if self.rect.colliderect(character.rect):
                 self.collected = True
-                character.chaves_coletadas += 1  # Incrementa o contador de chaves douradas coletadas no personagem
-    
-        if not self.collected and self.figura == "chave_azul.png":
+                character.chaves_coletadas += 1
+
+        if not self.collected and self.figura == 'chave_azul.png':
             if self.rect.colliderect(character.rect):
                 self.collected = True
-                character.chaves_azuis_coletadas += 1  # Incrementa o contador de chaves azuis coletadas no personagem
+                character.chaves_azuis_coletadas += 1
 
         if not self.collected:
-            if character.chaves_coletadas == 3 and character.chaves_azuis_coletadas == 3 and self.rect.colliderect(character.rect):
+            if character.chaves_coletadas == 1 and character.chaves_azuis_coletadas == 1 and self.rect.colliderect(
+                    character.rect):
                 self.collected = True
-                character.jaula_coletada += 1  # Incrementa o contador jaulas coletadas no personagem
+                character.jaula_coletada = True
 
     def draw(self, screen):
         if not self.collected:
             screen.blit(self.image, self.rect)
 
+class Ground(pg.sprite.Sprite):
+    def __init__(self, x, y, width, height):
+        super().__init__()
+        self.image = pg.Surface((width, height), pg.SRCALPHA)
+        self.image.fill((0, 0, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
 def main():
+    global sprites_behind_player
+    sprites_behind_player = pg.sprite.Group()
+
     pg.init()
     screen = pg.display.set_mode((1080, 760), pg.SCALED)
-    pg.display.set_caption("Llama simulator")
+    pg.display.set_caption('Llama simulator')
+    icon = pg.image.load('icon.png')
+    pg.display.set_icon(icon)
 
     width = screen.get_size()[0]
     height = screen.get_size()[1]
 
     background = pg.Surface(screen.get_size())
-    background = background.convert()
-    background = load_image('background.jpeg')
-    background = pg.transform.scale(background,(width,height))
+    background = load_image('background.png', scale=1)
+    background = pg.transform.scale(background, (width, height))
 
-    # chave
     keys_group = pg.sprite.Group()
-    key_gold1 = Key(600, 620, "chave_dourada.png", 0.25)  # Posição da chave
-    key_gold2 = Key(400, 620, "chave_dourada.png", 0.25)
-    key_gold3 = Key(500, 620, "chave_dourada.png", 0.25)
-    imagem_chave = Key(930, 30, "chave_dourada.png", 0.25)
+    chave_amarela = Key(700, 620, 'chave_dourada.png', 0.25)
+    chave_azul = Key(900, 620, 'chave_azul.png', 0.25)
+    jaula = Key(1000, 604, 'jaula.png', 1)
 
-    Key_blue1 = Key(800, 620, "chave_azul.png", 0.25)
-    Key_blue2 = Key(700, 620, "chave_azul.png", 0.25)
-    Key_blue3 = Key(900, 620, "chave_azul.png", 0.25)
-    imagem_chave_azul = Key(930, 70, "chave_azul.png", 0.25)
+    keys_group.add(chave_amarela, chave_azul, jaula)
 
-    jaula = Key(300, 600, "image-remove.png", 0.5)  # Posição da jaula
-    imagem_jaula = Key(920, 130, "image-remove.png", 0.5)
-    
-    keys_group.add(
-    key_gold1, key_gold2, key_gold3, imagem_chave,
-    Key_blue1, Key_blue2, Key_blue3, imagem_chave_azul,
-    jaula, imagem_jaula
-    )
+    # criação morangos
+    morangos = pg.sprite.Group()
+    morango1 = Morango(400, 620)
+    morango2 = Morango(400, 560)
+    morango3 = Morango(450, 560)
+    morango4 = Morango(450, 620)
+    morango5 = Morango(500, 620)
+    morango6 = Morango(500, 560)
+    morango7 = Morango(550, 560)
+    morango8 = Morango(550, 620)
+    morango9 = Morango(200, 560)
+    morango10 = Morango(200, 620)
+    morango11 = Morango(250, 560)
+    morango12 = Morango(250, 620)
+    morango13 = Morango(300, 560)
+    morango14 = Morango(300, 620)
+    morango15 = Morango(350, 560)
+    morango16 = Morango(350, 620)
+    morangos.add(morango1, morango2, morango3, morango4, morango5, morango6, morango7, morango8, morango9, morango10, morango11, morango12, morango13, morango14, morango15, morango16)
 
+    # criação do chão
+    grounds = pg.sprite.Group()
+    ground = Ground(0, 687, 1080, 70)
+    plataforma1 = Ground(500, 400, 50, 50)
+    grounds.add(ground)
 
     screen.blit(background, (0, 0))
     print(pg.display.get_surface().get_size())
     pg.display.flip()
 
-    pocoyo = Character()
-    allsprites = pg.sprite.RenderPlain((pocoyo))
+    fem_llama = None
+    contador_pulo = 0
+    llama = Character()
+
+    global allsprites
+    allsprites = pg.sprite.RenderPlain(llama)
     clock = pg.time.Clock()
+
+    hud = HUD(llama)
+    huds = pg.sprite.Group(hud)
 
     going = True
     while going:
@@ -183,39 +303,43 @@ def main():
         if pg.key.get_pressed()[pg.K_ESCAPE]:
             going = False
         elif going:
-            pocoyo.update(screen.get_size()[0], screen.get_size()[1])
+            llama.update(screen.get_size()[0], screen.get_size()[1], grounds)
 
-        allsprites.update(screen.get_size()[0], screen.get_size()[1])
-
-        keys_group.update(pocoyo)
-        keys_group.draw(screen)
+        keys_group.update(llama)
 
         screen.blit(background, (0, 0))
+
+        sprites_behind_player.draw(screen)
+
+        if llama.jaula_coletada:
+            if fem_llama != None:
+                if contador_pulo < 90:
+                    contador_pulo += 1
+                else:
+                    fem_llama._jump(screen.get_size()[1], grounds)
+            else:
+                fem_llama = FemLlama(965, 600)
+                sprites_behind_player.add(fem_llama)
+
         for key in keys_group:
             key.draw(screen)
-
+        allsprites.update(screen.get_size()[0], screen.get_size()[1], grounds)
         allsprites.draw(screen)
 
-        # Exibe a quantidade de chaves coletadas no canto superior direito da tela
-        mensagem = f'{pocoyo.chaves_coletadas}/3'
-        mensagem2 = f"{pocoyo.chaves_azuis_coletadas}/3"
-        fonte = pg.font.SysFont('Arial', 30)
-        texto_formatado = fonte.render(mensagem, True, (255, 255, 50))
-        texto_formatado2 = fonte.render(mensagem2, True, (150, 150, 150))
-        # Define a posição da frase no canto superior direito
-        screen.blit(texto_formatado, (950, 15))
-        screen.blit(texto_formatado2, (950, 55))
+        huds.update()
+        huds.draw(screen)
 
-        # Exibe a quantidade de jaulas coletadas no canto superior direito da tela
-        mensagem_jaula = f'{pocoyo.jaula_coletada}/1'
-        texto_jaula = fonte.render(mensagem_jaula, True, (255, 255, 50))
-        screen.blit(texto_jaula, (950, 110))
-        
+        grounds.draw(screen)
+
+        colisoes_morangos = pg.sprite.spritecollide(llama, morangos, True)
+        for morango in colisoes_morangos:
+            llama.morangos_coletados += 1
+
+        morangos.draw(screen)
+
         pg.display.flip()
 
     pg.quit()
 
-
 if __name__ == "__main__":
     main()
-
