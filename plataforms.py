7d2e275178bbe23debd12df15ab253877dbaf770
zrThawan <136650797+zrThawan@users.noreply.github.com>
2023-09-29 08:06:30 -0300

    deleting old files

--- plataforms.py
+++
@@ -1,8 +0,0 @@
-import pygame as pg
-
-class Plataform(pg.sprite.Sprite):
-    def __init__(self, x, y, width, height):
-        super().__init__()
-        self.image = pg.Surface((width, height), pg.SRCALPHA)
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y)
